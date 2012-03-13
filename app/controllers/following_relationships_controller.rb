0c62089f456397a20900b5d6d5ee6deac53c0412
Matthew Mongeau <halogenandtoast@gmail.com>
2012-03-13 12:24:16 -0400

    Finish mailers

--- app/controllers/following_relationships_controller.rb
+++ app/controllers/following_relationships_controller.rb
@@ -2,6 +2,7 @@ class FollowingRelationshipsController < ApplicationController
   def create
     user = User.find(params[:user_id])
     current_user.follow(user)
+    FollowingMailer.followed_notification(current_user, user).deliver
     redirect_to user, notice: "Now following"
   end
 end
