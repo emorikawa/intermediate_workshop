61a24c83f156f6dfeea72dd6bfcc143b7a019094
Matthew Mongeau <halogenandtoast@gmail.com>
2012-03-13 10:19:51 -0400

    Implement first side of following relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   include Clearance::User
   has_many :shouts
 
+  has_many :following_relationships, foreign_key: "following_user_id"
+  has_many :followed_users, through: :following_relationships
+
   def create_shout(medium)
     shouts.create(medium: medium)
   end
